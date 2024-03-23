ca6e2bcf28981c831c180a3f71c944defa09660b
Andrey Krutskikh <ppxxd@yandex.ru>
2024-03-23 22:21:22 +0300

    <feat>: Film and User classes; <feat>: UserController and FilmController for REST;

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -1,12 +1,31 @@
 package ru.yandex.practicum.filmorate.model;
 
-import lombok.Getter;
-import lombok.Setter;
+import lombok.Data;
+import lombok.NonNull;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Positive;
+import javax.validation.constraints.Size;
+import java.time.LocalDate;
 
 /**
  * Film.
  */
-@Getter
-@Setter
+@Data
 public class Film {
+    private int id;
+    @NonNull
+    @NotBlank
+    @NotEmpty
+    private String name;
+    @Size(max = 200)
+    @NonNull
+    private String description;
+    @NonNull
+//    @JsonDeserialize(using = LocalDateDeserializer.class)
+//    @JsonSerialize(using = LocalDateSerializer.class)
+    private LocalDate releaseDate;
+    @Positive
+    private int duration;
 }
